e9a42031a401cf2aa2cb1f4210a452894b9b9817
Densitty <dennis.brough@yandex.com>
2021-11-19 13:20:09 -0600

    setup the authentication context

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,13 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
+import React, { useContext, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const UserContext = React.createContext()
+const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
+  const data = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = data;
+
+  const [myUser, setMyUser] = useState(null);
+
+  useEffect(() => {
+    console.log("user is ", user);
+    console.log("isAuthenticated is ", isAuthenticated);
+    console.log("loading ", isLoading);
+  }, [isAuthenticated]);
+
   return (
-    <UserContext.Provider value='user context'>{children}</UserContext.Provider>
-  )
-}
+    <UserContext.Provider value={{ user, loginWithRedirect, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 // make sure use
 export const useUserContext = () => {
-  return useContext(UserContext)
-}
+  return useContext(UserContext);
+};
